0f0546b83efe0d7afc3287e9fc632b6df4793330
Dennisliu <dennis1224z@gmail.com>
2020-03-21 19:06:16 +0800

    calculate_AVG_length

--- r.py
+++ r.py
@@ -1,11 +1,17 @@
 data = []
 count = 0
+l = 0
 with open ('reviews.txt', 'r') as f:
 	for line in f:
 		count += 1
-		data.append(line.strip())
+		data.append(line)
+		l = l + len(line)
 		if count % 1000 == 0:
 			print(len(data))
-print(data[0])
-print('--------------------------')
-print(data[1])
+
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+sum_count = 0
+for d in data:
+	sum_count += len(d)
+
+print('留言平均長度為', sum_count / len(data))
